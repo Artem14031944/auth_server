cb893bd098a8414f9c92cd11c7bee1e375e6a5f1
artem14031944 <artem141221@gmail.com>
2025-03-11 18:39:29 +0300

    add Base oAuth service

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const tsconfig = require('./tsconfig.json');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -5,14 +7,10 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint/eslint-plugin',
-    // 'nestjs/recommended'
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'nestjs',
   ],
   root: true,
   env: {
@@ -25,5 +23,31 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Side-effect imports (e.g. polyfills or css imports)
+          ['^\\u0000'],
+          // Node.js core modules prefixed with `node:`
+          ['^node:'],
+          // External packages (npm modules)
+          ['^@?\\w'],
+          // Internal paths (your custom paths from tsconfig.json)
+          Object.keys(tsconfig.compilerOptions.paths),
+          // Relative imports (keep them at the bottom)
+          ['^[./]'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
   },
 };
